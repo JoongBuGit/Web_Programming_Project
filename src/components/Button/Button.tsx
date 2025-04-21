5c2a772e91faf72b108a927bf8263fa55ab7acfe
JoongBu <144006149+JoongBuGit@users.noreply.github.com>
2025-04-21 22:47:58 +0900

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import "@/components/Button/Button.css"
+import React from 'react';
+import "@/components/Button/Button.css";
 
+interface ButtonProps {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}
 
-export default function Buttion({onClick ,children}) {
+export default function Button({ onClick, children }: ButtonProps) {
   return (
-    <button className='button' onClick={onClick}>
+    <button className="button" onClick={onClick}>
       {children}
     </button>
-     
-  )
+  );
 }
