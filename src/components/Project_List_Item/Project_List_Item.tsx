e16cfe89b474e0862249efb764189278d3d4b2c1
JoongBu <144006149+JoongBuGit@users.noreply.github.com>
2025-04-21 22:53:37 +0900

    Update Project_List_Item.tsx

--- src/components/Project_List_Item/Project_List_Item.tsx
+++ src/components/Project_List_Item/Project_List_Item.tsx
@@ -1,36 +1,43 @@
-import Image from 'next/image'
-import React from 'react'
-import "@/components/Project_List_Item/Project_List_Item.css"
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
+import "@/components/Project_List_Item/Project_List_Item.css";
 
-export default function Project_List_Item({ id }) {
+interface ProjectListItemProps {
+  id: string;
+}
 
-  let project_id = id;
-
+export default function Project_List_Item({ id }: ProjectListItemProps) {
   return (
-    <div >
+    <div>
       <div className="clear">
-        <div className='item_container'>
-
-          <Link href={`/project?project_id=${project_id}`}>
-
-          <div className="thumbnail_left">
-            <Image src={"/image/project1_thumb.png"} width={300} height={300} alt="project image"></Image>
-            <div className="text_of_thumbnail">
-            <br></br>
-              <div>프로젝트 제목 {project_id}</div>
-              <br></br>
-              <p>프로젝트 설명 부분 어떤 프로젝트인지 간단하게 설명하는 부분이다</p>
+        <div className="item_container">
+          <Link href={`/project?project_id=${id}`}>
+            <div className="thumbnail_left">
+              <Image
+                src="/image/project1_thumb.png"
+                width={300}
+                height={300}
+                alt="project image"
+              />
+              <div className="text_of_thumbnail">
+                <div style={{ marginTop: '1rem' }}>
+                  프로젝트 제목 {id}
+                </div>
+                <p style={{ margin: '1rem 0' }}>
+                  프로젝트 설명 부분 어떤 프로젝트인지 간단하게 설명하는 부분이다
+                </p>
+              </div>
             </div>
-          </div>
-          
-          <Image className="thumbnail_right" src={"/image/project1_img.png"} width={800} height={500} alt="project image"></Image>
+            <Image
+              className="thumbnail_right"
+              src="/image/project1_img.png"
+              width={800}
+              height={500}
+              alt="project image"
+            />
           </Link>
-         
-
         </div>
       </div>
-
     </div>
-  )
+  );
 }
